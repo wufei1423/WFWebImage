1189214e2bdeb535a71a53a8296bf7bfb60dcfe5
wufei1423 <wufei1423@163.com>
2015-08-29 09:26:50 +0800

    init

---
+++ WFWebImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WFWebImage
+//
+//  Created by feiwu on 15/8/29.
+//  Copyright (c) 2015年 feiwu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WFWebImage/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WFWebImage
+//
+//  Created by feiwu on 15/8/29.
+//  Copyright (c) 2015年 feiwu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
